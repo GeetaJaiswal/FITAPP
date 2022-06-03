188cdc47238a861c66f292460fe9b482845f8178
Geeta <geetaverma6653@gmail.com>
2022-06-03 17:28:21 +0530

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://FitnessUser:Fitness123@cluster0.7xnxx.mongodb.net/Fitness?retryWrites=true&w=majority",{
+mongoose.connect(process.env.DB,{
     useNewUrlParser:true,
     useUnifiedTopology:true 
 }).then(()=>{
