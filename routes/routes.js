ea5e7921e92a770693c4887e53f3a2a375b379b1
Geeta <geetaverma6653@gmail.com>
2022-06-03 21:43:40 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -56,7 +56,7 @@ router.post("/paynow", [parseUrl, parseJson], async (req, res) => {
     params['ORDER_ID'] = 'TEST_' + new Date().getTime();
     params['CUST_ID'] = paymentDetails.customerId;
     params['TXN_AMOUNT'] = paymentDetails.amount;
-    params['CALLBACK_URL'] = 'http://localhost:5000/callback';
+    params['CALLBACK_URL'] = 'https://feel-fit.herokuapp.com/checkout';
     params['EMAIL'] = paymentDetails.customerEmail;
     params['MOBILE_NO'] = paymentDetails.customerPhone;
 
